f75f5193b5f576da8c9698a91d439f0e3eb34f58
Piers <piers@eightytwo.com.au>
2013-06-04 00:37:18 +0000

    Remove delay when ESC key is pressed
    By default there is a delay of 1000 milliseconds
    when the escape key is pressed in case an escape
    sequence is being entered. This is not needed.
    
    Comments have also been changed to be consistent
    with the rest of the code base.

--- src/client.c
+++ src/client.c
@@ -3,7 +3,7 @@
 #include "client.h"
 #include "log.h"
 
-// Global variables
+/* global variables */
 int command_mode = 0;
 int input_height = 1;
 size_t position = 0;
@@ -11,7 +11,7 @@ static char command_buffer[1024];
 WINDOW *main_window;
 WINDOW *input_window;
 
-// Function prototypes
+/* function prototypes */
 WINDOW *create_window(int height, int width, int starty, int startx);
 void handle_input();
 void init_curses();
@@ -22,12 +22,12 @@ void resize_view();
 int ant_client_start(void) {
    int ch;
 
-   // Initialise the curses environment
+   /* initialise the curses environment */
    init_curses();
 
-   // Endlessly process user input and bail out when escape is pressed.
-   // Pressing escape while in command mode will exit command mode, not the
-   // program.
+   /* Endlessly process user input and bail out when escape is pressed.
+    * Pressing escape while in command mode will exit command mode, not the
+    * program. */
    while ((ch = getch()) != 27 || command_mode == 1) {
       handle_input(ch);
    }
@@ -50,12 +50,15 @@ void init_curses() {
    noecho();
    keypad(stdscr, TRUE);
 
-   // Create the main and input windows
+   /* no delay when escape is pressed */
+   ESCDELAY = 0;
+
+   /* create the main and input windows */
    main_height = LINES - input_height;
    main_window = create_window(main_height, COLS, 0, 0);
    input_window = create_window(input_height, COLS, main_height, 0);
 
-   // Set up the window colours if supported
+   /* set up the window colours if supported */
    if (has_colors()) {
       start_color();
       use_default_colors();
@@ -65,7 +68,7 @@ void init_curses() {
       wbkgd(input_window, COLOR_PAIR(2));
    }
 
-   // Ensure the curses changes are realised
+   /* ensure the curses changes are realised */
    refresh_view();
 }
 
@@ -80,36 +83,36 @@ void refresh_view() {
 void resize_view() {
    int height, width;
 
-   // Get the new dimensions of the screen
+   /* get the new dimensions of the screen */
    getmaxyx(stdscr, height, width);
 
-   // Resize the main window
+   /* resize the main window */
    wresize(main_window, height - input_height, width);
    mvwin(main_window, 0, 0);
 
-   // Resize the input window
+   /* resize the input window */
    wresize(input_window, input_height, width);
    mvwin(input_window, height - input_height, 0);
 
-   // Refresh the entire view
+   /* refresh the entire view */
    refresh_view();
 }
 
 /* Processes user input */
 void handle_input(int ch) {
-   // Check if the character entered is a backspace
+   /* check if the character entered is a backspace */
    if (ch == erasechar()) {
       ch = KEY_BACKSPACE;
    }
 
    switch(ch) {
       case ERR:
-         // -1 is sent when there is no input, e.g. when the terminal
-         // is being resized. Nothing to do.
+         /* -1 is sent when there is no input, e.g. when the terminal
+          * is being resized. Nothing to do. */
          break;
 
       case 27:
-         // Escape pressed so exit command mode
+         /* escape pressed so exit command mode */
          if (command_mode) {
             command_mode = 0;
             wclear(input_window);
@@ -118,14 +121,14 @@ void handle_input(int ch) {
          break;
 
       case KEY_RESIZE:
-         // The terminal is being resized
+         /* the terminal is being resized */
          resize_view();
          break;
 
       case ':':
          wechochar(input_window, ch);
 
-         // Switch to command mode
+         /* switch to command mode */
          if (!command_mode) {
             command_mode = 1;
          } else {
@@ -135,14 +138,14 @@ void handle_input(int ch) {
 
       case KEY_BACKSPACE:
          if (position > 0) {
-            // Move the cursor back one character
+            /* move the cursor back one character */
             wmove(input_window, 0, position);
 
-            // Remove the character from the buffer
+            /* remove the character from the buffer */
             command_buffer[--position] = 0;
 
-            // Delete the character from the window under the cursor
-            // and refresh the window
+            /* delete the character from the window under the cursor
+             * and refresh the window */
             wdelch(input_window);
             wrefresh(input_window);
          }
@@ -150,19 +153,19 @@ void handle_input(int ch) {
 
       case KEY_ENTER:
       case '\n':
-         // Terminate the command string and reset the command variables
+         /* terminate the command string and reset the command variables */
          command_buffer[position++] = 0;
          position = 0;
          command_mode = 0;
 
-         // Reset the input window
+         /* reset the input window */
          wclear(input_window);
          wrefresh(input_window);
          ant_log("Entered command: %s", command_buffer);
          break;
 
       default:
-         // Echo the character if command mode is active
+         /* echo the character if command mode is active */
          if (command_mode == 1) {
             command_buffer[position++] = (char)ch;
             wechochar(input_window, ch);
